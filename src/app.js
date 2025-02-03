04f760b5ad322f11d61fe6dd248bed05ecee18d3
Maxime L <maxime30labbe@gmail.com>
2025-02-03 17:00:06 +0100

    Fix: Fixed app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const NodeCache = require("node-cache");
+const getAssessments = require("./get-assessments");
+const getSpeciesInformations = require("./get-species-informations");
 
 const app = express();
 const PORT = process.env.PORT || 3001; // Changez le port ici
@@ -16,6 +18,19 @@ app.get("/countries/:countryCode", async (req, res) => {
         const countryCode = req.params.countryCode;
         const species = await getAssessments(countryCode, TOKEN);
         res.json(species);
+        const assessments = await getAssessments(countryCode, TOKEN);
+        // Récupération des détails pour chaque sis_taxon_id
+        const enrichedAssessments = await Promise.all(
+            assessments.assessments.map(async (assessment) => {
+                if (assessment.sis_taxon_id) {
+                    const speciesDetails = await getSpeciesInformations(assessment.sis_taxon_id, TOKEN);
+                    const speciesName = speciesDetails.taxon.common_names.filter(name => name.language === "fre")[0];
+                    return speciesName ? speciesName.name : speciesDetails.taxon.scientific_name ;
+                }
+                return assessment;
+            })
+        );
+        res.json({ names: enrichedAssessments });
     } catch (error) {
         console.error("Erreur:", error);
         res.status(500).send("Erreur lors de la récupération des espèces.");
@@ -41,6 +56,7 @@ app.get("/", async (req, res) => {
             <html>
             <head>
                 <title>Résultats IUCN par pays</title>
+                <link href="home.css" rel="stylesheet"/>
                 <style>
                     #countrySelect {
                         max-height: 300px;
@@ -52,7 +68,8 @@ app.get("/", async (req, res) => {
                         try {
                             const response = await fetch('/countries/' + countryCode);
                             const species = await response.json();
-                            console.log("Species for country code", countryCode, ":", species);
+                            const speciesList = species.names.sort((a, b) => a.localeCompare(b));
+                            document.getElementById('speciesList').innerText = JSON.stringify(speciesList, null, 2);
                         } catch (error) {
                             console.error("Erreur:", error);
                         }
@@ -60,9 +77,12 @@ app.get("/", async (req, res) => {
                 </script>
             </head>
             <body>
+                <h1 class='title'>Résultats IUCN par pays</h1>
                 <select id="countrySelect" onchange="handleClick(this.value)">
                     ${countries.map(country => `<option value="${country.cca2}">${country.translations.fra.common}</option>`).join('')}
                 </select>
+                <h2>Liste des espèces en danger</h2>
+                <pre id="speciesList"></pre>
             </body>
             </html>
         `;
